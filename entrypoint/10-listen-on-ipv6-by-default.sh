808caadf81379a4ee4939e424c73c28ea14d0812
Dale Smith <dale@catalystcloud.nz>
2026-01-06 06:46:16 +1300

    fix: Use temp file for ipv6 conf checksum (#228)
    The conf file was modified to change listen port in the Dockerfile,
    so to correctly compare packaged checksums we create a temporary
    file with the known changes reverted.
    
    This resolves an issue where the container would never listen on
    IPv6 on startup.

--- entrypoint/10-listen-on-ipv6-by-default.sh
+++ entrypoint/10-listen-on-ipv6-by-default.sh
@@ -37,18 +37,21 @@ else
 fi
 
 entrypoint_log "$ME: info: Getting the checksum of /$DEFAULT_CONF_FILE"
+# Use a temporary file to undo the port change so checksums can be compared.
+CONFTMP=$(mktemp)
+cat "/$DEFAULT_CONF_FILE" | sed 's,listen       8080;,listen       80;,' > $CONFTMP
 
 case "$ID" in
     "debian")
         CHECKSUM=$(dpkg-query --show --showformat='${Conffiles}\n' nginx | grep $DEFAULT_CONF_FILE | cut -d' ' -f 3)
-        echo "$CHECKSUM  /$DEFAULT_CONF_FILE" | md5sum -c - >/dev/null 2>&1 || {
+        echo "$CHECKSUM  $CONFTMP" | md5sum -c - >/dev/null 2>&1 || {
             entrypoint_log "$ME: info: /$DEFAULT_CONF_FILE differs from the packaged version"
             exit 0
         }
         ;;
     "alpine")
         CHECKSUM=$(apk manifest nginx 2>/dev/null| grep $DEFAULT_CONF_FILE | cut -d' ' -f 1 | cut -d ':' -f 2)
-        echo "$CHECKSUM  /$DEFAULT_CONF_FILE" | sha1sum -c - >/dev/null 2>&1 || {
+        echo "$CHECKSUM  $CONFTMP" | sha1sum -c - >/dev/null 2>&1 || {
             entrypoint_log "$ME: info: /$DEFAULT_CONF_FILE differs from the packaged version"
             exit 0
         }
@@ -58,6 +61,7 @@ case "$ID" in
         exit 0
         ;;
 esac
+rm "$CONFTMP"
 
 # enable ipv6 on default.conf listen sockets
 sed -i -E 's,listen       8080;,listen       8080;\n    listen  [::]:8080;,' /$DEFAULT_CONF_FILE
